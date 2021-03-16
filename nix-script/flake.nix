44ef383d6fd0391870a6eb086855cc5a95717b16
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-15 19:58:12 -0700

    flake: nix-script

--- nix-script/flake.nix
+++ nix-script/flake.nix
@@ -5,9 +5,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/3a7674c896847d18e598fa5da23d7426cb9be3d2";
     easy-hls-nix = { url = "github:jkachmar/easy-hls-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nix-script = { url = "github:BrianHicks/nix-script"; inputs.nixpkgs.follows = "nixpkgs"; flake = false; };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, easy-hls-nix }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, easy-hls-nix, nix-script }:
     { }
     //
     (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
@@ -26,6 +27,9 @@
           devShell = with pkgs; mkShell {
             buildInputs = [
               easy-hls
+              nix-script-bin
+              nix-script-haskell
+              nix-script-bash
               (haskellPackages.ghcWithPackages
                 (p: with p;  [
                   relude
@@ -37,6 +41,9 @@
     ) //
     {
       overlay = final: prev: {
+        nix-script-bin = prev.callPackage "${nix-script}/nix-script" { };
+        nix-script-haskell = prev.callPackage "${nix-script}/nix-script-haskell" { };
+        nix-script-bash = prev.callPackage "${nix-script}/nix-script-bash" { };
         easy-hls = prev.callPackage easy-hls-nix {
           ghcVersions = [ "8.10.4" ];
         };
