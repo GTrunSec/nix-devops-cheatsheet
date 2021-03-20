d7d3c2b33c1796325bfb3cbd98c3e48139ea4514
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-20 15:34:11 -0700

    Sat Mar 20 15:34:13 PDT 2021

--- python/flake.nix
+++ python/flake.nix
@@ -5,12 +5,16 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/3a7674c896847d18e598fa5da23d7426cb9be3d2";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db/2205d5a0fc9b691e7190d18ba164a3c594570a4b";
+      flake = false;
+    };
     mach-nix = { url = "github:DavHau/mach-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, mach-nix }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, flake-compat, mach-nix, pypi-deps-db }:
     {
-      python-dev-node-packages = import ./pythonDev-node-packages;
+      node-packages = import ./node-packages;
     }
     //
     (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
@@ -18,31 +22,46 @@
         let
           machLib = import mach-nix
             {
-              pypiDataRev = "2205d5a0fc9b691e7190d18ba164a3c594570a4b";
-              pypiDataSha256 = "1aaylax7jlwsphyz3p73790qbrmva3mzm56yf5pbd8hbkaavcp9g";
+              pypiDataRev = pypi-deps-db.rev;
+              pypiDataSha256 = pypi-deps-db.narHash;
               python = "python39";
             };
+
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
-              self.python-dev-node-packages
+              self.node-packages
               self.overlay
             ];
             config = { };
           };
         in
         rec {
+
           python-packages-custom = machLib.mkPython rec {
             requirements = ''
               isort
             '';
           };
+
           devShell = with pkgs; mkShell {
             buildInputs = [
               python-packages-custom
               nodePackages.pyright
               nodePackages.node2nix
             ];
+          };
+
+          packages = {
+            inherit (pkgs.nodePackages)
+              pyright
+              node2nix
+              ;
+            pthon-packages = python-packages-custom;
+          };
+
+          hydraJobs = {
+            inherit packages;
           };
         }
       )
