4f74115bc6d3ae3e617458c096c7904629b5d757
GTrunSec <gtrunsec@hardenedlinux.org>
2021-04-15 15:34:27 -0700

    Thu Apr 15 15:34:28 PDT 2021

--- rust/flake.nix
+++ rust/flake.nix
@@ -4,28 +4,26 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/7d71001b796340b219d1bfa8552c81995017544a";
-    master.url = "nixpkgs";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, flake-compat, master, rust-overlay }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, flake-compat, rust-overlay }:
     { }
     //
     (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
       (system:
         let
-          unstable = final: prev: {
-            inherit ((import inputs.master) { inherit system; })
-              rustracer;
-          };
-
+          # unstable = final: prev: {
+          #   inherit ((import inputs.master) { inherit system; })
+          #     rustracer;
+          # };
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
               self.overlay
               (import rust-overlay)
-              unstable
+              #unstable
             ];
 
             config = { };
@@ -44,15 +42,19 @@
               buildInputs = [
                 rustracer
               ] ++ (with rust-bin.nightly.latest; [
-                rust
+                rust-final
               ]);
-              RUST_SRC_PATH = "${rust-bin.nightly.rust-src}/lib/rustlib/src/rust/library";
+              RUST_SRC_PATH = "${rust-final}/lib/rustlib/src/rust/library";
               shellHook = ''
             '';
             };
         })
     ) //
     {
-      overlay = final: prev: { };
+      overlay = final: prev: {
+        rust-final = final.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      };
     };
 }
