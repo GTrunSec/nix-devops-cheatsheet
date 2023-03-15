484d856343f38eca2b1d26097dcdd97493489db4
guangtao <gtrunsec@hardenedlinux.org>
2023-03-14 22:23:08 -0700

    python: add overrides example

--- python/poetry/default.nix
+++ python/poetry/default.nix
@@ -7,6 +7,7 @@ poetry2nix.mkPoetryEnv {
   projectDir = ./.;
   preferWheels = true;
   python = python3;
+  # overrides = import ./overrides-preferWheels.nix poetry2nix;
   extraPackages = ps: [
     # ps.python-lsp-server
     # ps.python-lsp-server.passthru.optional-dependencies.all
